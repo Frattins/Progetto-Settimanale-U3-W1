f91eb2897e662428012cdcd365133e26ccd60562
Frattins <davide.frattini1@gmail.com>
2024-05-24 11:27:36 +0200

    Revert "routing"
    This reverts commit fe1630d9ec73ed604f34be1c2b3702a7e7cab31a.

--- autoSalone/src/app/app-routing.module.ts
+++ autoSalone/src/app/app-routing.module.ts
@@ -1,37 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { FordComponent } from './ford/ford.component';
-import { AudiComponent } from './audi/audi.component';
-import { FiatComponent } from './fiat/fiat.component';
 
-const routes: Routes = [
-  {
-    path:'',
-    redirectTo:'/home',
-    pathMatch: 'full'
-  },
-  {
-    path:'home',
-    component: HomeComponent,
-    title:'Home'
-  },
-  {
-    path: 'ford',
-    component: FordComponent,
-    title: 'Ford',
-  },
-  {
-    path: 'audi',
-    component: AudiComponent,
-    title: 'Audi',
-   },
-   {
-    path: 'fiat',
-    component: FiatComponent,
-    title: 'Fiat',
-   }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- autoSalone/src/app/app.module.ts
+++ autoSalone/src/app/app.module.ts
@@ -5,20 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './mainComponent/navbar/navbar.component';
 import { FooterComponent } from './mainComponent/footer/footer.component';
-import { FiatComponent } from './fiat/fiat.component';
-import { FordComponent } from './ford/ford.component';
-import { AudiComponent } from './audi/audi.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent,
-    FiatComponent,
-    FordComponent,
-    AudiComponent,
-    HomeComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
